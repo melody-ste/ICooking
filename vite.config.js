c15b30f1ba36aba6b6c73f8a8c925bc41cde49df
melody-ste <melody_stephan@icloud.com>
2025-09-05 11:37:49 +0200

    adding gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base : "/ICooking/",
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: "autoUpdate",
+      manifest: {
+        name: "My Vite React PWA",
+        short_name: "ICooking",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#ffffff",
+      }
+    })
+  ]
+});
